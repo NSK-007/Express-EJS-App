b981f416cab1dadf914cdd6060320b2e49ba525b
shiva-kumar-nadigoti <nshiva900@gmail.com>
2023-02-19 19:54:49 +0530

    added dynamic routes

--- models/product.js
+++ models/product.js
@@ -1,33 +1,48 @@
 const fs = require('fs');
 const path = require('path');
-const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
-// const products = [];
 
-const getProductsFromFile = (cb) => {
-    fs.readFile(p, (err, fileContent) => {
-        if(err)
-            cb([]);
-        cb(JSON.parse(fileContent));
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    this.id = Math.random().toString();
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+
+  static findById(id, cb){
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
+      cb(product);
     })
-}
-
-module.exports = class Product{
-    
-    constructor(title){
-        this.title = title;
-    }
-
-    save(){
-        // products.push(this);
-        getProductsFromFile(products => {
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log(err);
-            });
-        });
-    }
-
-    static fetchAll(cb){
-        getProductsFromFile(cb);
-    }
-}
+  }
+};
